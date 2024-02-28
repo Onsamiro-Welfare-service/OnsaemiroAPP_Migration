026450376a378341ddf7114329402eedf017272e
TopazKang <lasksla@gmail.com>
2024-02-28 18:57:00 +0900

    category

--- lib/screens/categoryScreen.dart
+++ lib/screens/categoryScreen.dart
@@ -1,40 +1,79 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:test_onsaemiro_0/route.dart';
 
-class CategoryScreen extends StatelessWidget {
-  const CategoryScreen({super.key});
+class CategoryScreen extends StatefulWidget {
+  const CategoryScreen({Key? key}) : super(key:key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text("Card Sample")),
-        body: const CardExample(),
-      ),
-
-    );
-  }
+  _CategoryRead createState() => _CategoryRead();
 }
 
-class CardExample extends StatelessWidget {
-  const CardExample({super.key});
+class _CategoryRead extends State<CategoryScreen>{
+
+  @override //빌드와 함께 카테고리 정보를 불러오기 위함.
+  void initState(){
+    _Category(context);
+    print(decodedCategoryList);
+    super.initState();
+  }
+
+  var decodedCategoryList = [];
+
+  Future<void> _Category(BuildContext context) async {
+
+    final prefs = await SharedPreferences.getInstance(); // SharedPreferences 인스턴스 세팅
+
+    final accessToken = prefs.getString('AccessTk'); //토큰 조회
+    var url = Uri.parse(Routes.getCategory);
+    var response = await http.get(url,
+        headers: {'Content-Type' : 'application/json',
+        'Authorization': 'Bearer ${accessToken}'},
+    );
+
+    print('Response status: ${response.statusCode}'); // 요청에 대한 응답상태를 불러오기 위함
+    print('Response body: ${response.body}'); // 받아온 요청 값
+
+    if (response.statusCode == 403){
+      print('Category Read error');
+    }else if (response.statusCode == 200){
+
+     var encodedCategoryList = response.body; // String 형식( SharedPreferences가 리스트<dynamic> 타입을 받지 못해서 )
+     decodedCategoryList = jsonDecode(response.body)["categoryList"]; // List<dynamic> 형식 => 실제 사용을 위함.
+
+     prefs.setString('categoryList', encodedCategoryList); //내부 저장소에 저장( json형태로 저장 )
+     // final categoryList = prefs.getString('categoryList'); // 내부 저장소에서 조회( 사용시 decode 필수 )
+
+      print('saved categoryList: $decodedCategoryList');
+    }
+
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-        clipBehavior: Clip.hardEdge,
-        child: InkWell(
-          splashColor: Colors.blue.withAlpha(30),
-          onTap: () {
-            debugPrint('CardTapped.');
-          },
-          child: const SizedBox(
-            width: 300,
-            height: 100,
-            child: Text('A card')
-          )
+    return Scaffold(
+        body: ListView.builder(
+            itemCount: decodedCategoryList.length,
+            itemBuilder: (context, index){
+              return  Card(
+                  clipBehavior: Clip.hardEdge,
+                  child: InkWell(
+                      splashColor: Colors.blue.withAlpha(30),
+                      onTap: () {
+                        _Category(context);
+                      },
+                      child: SizedBox(
+                          width: 300,
+                          height: 100,
+                          child: Text(decodedCategoryList[index]["name"].toString())
+                      )
+                  )
+              );
+            }
         )
-      )
     );
   }
 }
+
--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -84,3 +84,4 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 }
+
